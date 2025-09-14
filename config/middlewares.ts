1f94d5a9db785e76ab8f07c0001228aa1cb49768
beamed8 <haivron@gmail.com>
2025-09-14 03:10:01 +0300

    ram usage debugger

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,21 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+
+  // Custom middleware for memory usage
+  async (ctx, next) => {
+    await next();
+
+    const mem = process.memoryUsage();
+    const usedMB = (mem.rss / 1024 / 1024).toFixed(2);
+    strapi.log.info(`Memory usage (RSS): ${usedMB} MB`);
+  },
 ];
